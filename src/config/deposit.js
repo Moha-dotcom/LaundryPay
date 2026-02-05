f0644b73109cb32c10e84287fa86bcc882be41d7
Moha-dotcom <Mohamedsahal612@gmail.com>
2026-02-04 20:30:32 -0600

    Feat : Implement Deposit Logs. -- Now we can Store Every Deposit in the Deposit table.

--- src/config/deposit.js
+++ src/config/deposit.js
@@ -1,5 +1,5 @@
 import pool from '../config/dbconnect.js'
-import user from "../data/user.js";
+
 const log = console.log
 class Deposit {
     constructor(user_id,  deposit_amount, account_id) {
@@ -9,38 +9,18 @@ class Deposit {
     }
     get deposit_amount() { return this._deposit_amount; }
     get account_id() { return this._account_id; }
-    // async set deposit( amount ) {
-    //     // connect to the Database
-    //     const client = await pool.connect();
-    //     // Deposit Process
-    //     // Get the Balance on the account of the user
-    //     // then Update the balance
-    //     // then record that balance on the deposit table
-    //
-    //
-    //
-    // }
 
-    static async setDeposit({user_id, account_id, deposit_amount}) {
+    async setDeposit({user_id, account_id, deposit_amount}) {
         const client = await pool.connect();
         try {
             await client.query('BEGIN');
-
-            await client.query(
-                `SELECT set_config('app.current_user_id', $1, true)`,
-                [String(user_id)],
-            );
+            await this.setCurrentUser(client, user_id);
             // 4️⃣ Apply deposit (RLS enforced here)
-            const updateResult = await client.query(
-                `UPDATE accounts
-             SET balance = balance + $1,
-                 updateAt = now()
-             WHERE id = $2
-             RETURNING balance, updateAt`,
-                [Number(deposit_amount), Number(account_id)]
-            );
+            const updateResult = await this.updateUserAccountWithNewDepositAmount(client, deposit_amount, account_id);
+            let res = await this.storeDepositLogs(client, deposit_amount, account_id);// If deposit is Less than 0 Don't store
             await client.query('COMMIT');
-            return updateResult.rows[0];
+            log(updateResult.rows[0])
+            return res.rows[0]
 
         }catch (error) {
             console.log(error);
@@ -49,14 +29,33 @@ class Deposit {
         }
     }
 
-    storeDepositLogs(deposit_amount, account_id) {
-
+     async storeDepositLogs(client, deposit_amount, account_id) {
+        if(deposit_amount < 1)  throw new Error ('Deposit amount should be greater than 0');
+        return await client.query(`INSERT INTO DEPOSITS (deposited_amount, account_id) VALUES ($1, $2)  RETURNING deposited_amount `, [Number(deposit_amount), Number(account_id)])
     }
+
+
+     async setCurrentUser(client, user_id) {
+        await client.query(
+            `SELECT set_config('app.current_user_id', $1, true)`,
+            [String(user_id)],
+        );
+    }
+
+     async updateUserAccountWithNewDepositAmount(client, deposit_amount, account_id) {
+        return await client.query(
+            `UPDATE accounts
+             SET balance = balance + $1,
+                 updateAt = now()
+             WHERE id = $2
+             RETURNING balance, updateAt`,
+            [Number(deposit_amount), Number(account_id)]
+        );
+    }
+
 }
 
-//
-
-const deposits = new Deposit(2, 12, 4)
-Deposit.setDeposit(deposits).then(r => log(r)).catch(err => log(err.message));
+const deposits = new Deposit(2, 192, 4)
+deposits.setDeposit(deposits).then(r => log(r)).catch(err => log(err.message));
 
 
