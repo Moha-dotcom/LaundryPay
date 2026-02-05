00ba9735c24c232beb8394caab227546afac7119
Moha-dotcom <Mohamedsahal612@gmail.com>
2026-02-05 12:04:36 -0600

    fix :Refactor setDeposit to use instance state instead of passing parameters.  - This improves Encapsulation removes redundant parameter passing

--- src/config/deposit.js
+++ src/config/deposit.js
@@ -3,21 +3,22 @@ import pool from '../config/dbconnect.js'
 const log = console.log
 class Deposit {
     constructor(user_id,  deposit_amount, account_id) {
-        this.user_id = user_id;
+        this._user_id = user_id;
         this._deposit_amount = deposit_amount;
         this._account_id = account_id;
     }
-    get deposit_amount() { return this._deposit_amount; }
-    get account_id() { return this._account_id; }
+    getDeposit_amount() { return this._deposit_amount; }
+    getAccount_id() { return this._account_id; }
+    getUserID(){return this._user_id}
 
-    async setDeposit({user_id, account_id, deposit_amount}) {
+    async setDeposit() {
         const client = await pool.connect();
         try {
             await client.query('BEGIN');
-            await this.setCurrentUser(client, user_id);
+            await this.setCurrentUser(client, this.getUserID());
             // 4️⃣ Apply deposit (RLS enforced here)
-            const updateResult = await this.updateUserAccountWithNewDepositAmount(client, deposit_amount, account_id);
-            let res = await this.storeDepositLogs(client, deposit_amount, account_id);// If deposit is Less than 0 Don't store
+            const updateResult = await this.updateUserAccountWithNewDepositAmount(client, this.getDeposit_amount(), this.getAccount_id());
+            let res = await this.storeDepositLogs(client, this.getDeposit_amount(), this.getAccount_id());// If deposit is Less than 0 Don't store
             await client.query('COMMIT');
             log(updateResult.rows[0])
             return res.rows[0]
@@ -28,6 +29,7 @@ class Deposit {
             await client.release();
         }
     }
+
 
      async storeDepositLogs(client, deposit_amount, account_id) {
         if(deposit_amount < 1)  throw new Error ('Deposit amount should be greater than 0');
@@ -55,7 +57,7 @@ class Deposit {
 
 }
 
-const deposits = new Deposit(2, 192, 4)
+const deposits = new Deposit(2, 10, 4)
 deposits.setDeposit(deposits).then(r => log(r)).catch(err => log(err.message));
 
 
